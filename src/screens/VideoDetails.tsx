54b5f3bd9440f65f348447c7426b8175cef58c81
Aleyna Aktaş <aleynaaktas627@gmail.com>
2024-05-13 17:43:24 +0300

    Add video viewing for Videos Screen

--- src/screens/VideoDetails.tsx
+++ src/screens/VideoDetails.tsx
@@ -1,14 +1,8 @@
-import React, {useRef, useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-  Text,
-  Modal,
-  Platform,
-} from 'react-native';
-import Video from 'react-native-video';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
 import {Route, useRoute} from '@react-navigation/native';
+import Video from 'react-native-video';
+import VideoPlayer from 'react-native-video-controls';
 
 export interface VideoDetails {
   id: number;
@@ -20,80 +14,35 @@ export interface VideoDetails {
 type VideoDetailsRoute = Route<'VideoDetails', {title: string; item?: any}>;
 
 const VideoDetails = () => {
-  const player = useRef(null);
   const route = useRoute<VideoDetailsRoute>();
   const videoUri = route.params.item.videoUri;
-  const [isPlaying, setIsPlaying] = useState(true);
-  const [isFullScreen, setIsFullScreen] = useState(false);
-  const [buffering, setBuffering] = useState(false);
-  const [error, setError] = useState(null);
-
-  const onBuffer = ({isBuffering}: any) => {
-    setBuffering(isBuffering);
-  };
-
-  const videoError = (error: any) => {
-    console.error('Video error:', error);
-    setError(error);
-  };
-
-  const togglePlayback = () => {
-    setIsPlaying(!isPlaying);
-  };
-
-  const toggleFullScreen = () => {
-    setIsFullScreen(!isFullScreen);
-  };
+  const [isVideoOpen, setIsVideoOpen] = React.useState(false);
 
   return (
-    <View style={styles.container}>
-      <Modal
-        animationType="slide"
-        transparent={false}
-        visible={isFullScreen}
-        onRequestClose={toggleFullScreen}
-        supportedOrientations={['landscape']}>
-        <View style={styles.fullScreenContainer}>
-          <Video
-            source={{uri: videoUri}}
-            ref={player}
-            onBuffer={onBuffer}
-            onError={videoError}
-            style={styles.fullScreenVideo}
-            resizeMode="contain"
-            paused={!isPlaying}
-            onFullscreenPlayerWillDismiss={toggleFullScreen}
-          />
-        </View>
-      </Modal>
-      <Video
-        source={{uri: videoUri}}
-        ref={player}
-        onBuffer={onBuffer}
-        onError={videoError}
-        style={styles.backgroundVideo}
+    <>
+      <VideoPlayer
         resizeMode="contain"
-        paused={!isPlaying}
-        onFullscreenPlayerWillDismiss={toggleFullScreen}
+        fullscreen
+        fullscreenOrientation="all"
+        // title={userStore.info.introduction_video.split('/files/')[1].split('.mp4')[0]}
+        controlAnimationTiming={200}
+        showTimeRemaining={false}
+        controlTimeout={5000}
+        onBack={() => setIsVideoOpen(false)}
+        // navigator={navigation}
+        source={{uri: videoUri}}
+        style={styles.video}
       />
-      <View style={styles.controls}>
-        <TouchableOpacity style={styles.button} onPress={togglePlayback}>
-          <Text style={styles.buttonText}>{isPlaying ? 'Pause' : 'Play'}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={toggleFullScreen}>
-          <Text style={styles.buttonText}>Fullscreen</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // backgroundColor: 'black',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
   backgroundVideo: {
     position: 'absolute',
@@ -101,31 +50,23 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
+    zIndex: 1,
   },
-  fullScreenContainer: {
-    flex: 1,
-    backgroundColor: 'black',
+  video: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    zIndex: 1000,
   },
-  fullScreenVideo: {
-    flex: 1,
-  },
-  controls: {
-    flexDirection: 'row',
+  videoContainer: {
+    width: '100%',
+    height: 180,
+    marginTop: 22,
+    marginBottom: 20,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'transparent',
-    position: 'absolute',
-    bottom: 20,
-  },
-  button: {
-    padding: 10,
-    margin: 10,
-    backgroundColor: '#FFFFFFA0',
-    borderRadius: 10,
-  },
-  buttonText: {
-    fontSize: 16,
-    color: 'black',
   },
 });
 
