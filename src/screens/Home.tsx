cb847a700b7c879515f07268b61925030701a193
Aleyna Aktaş <aleynaaktas627@gmail.com>
2024-09-26 23:47:47 +0300

    Edit subjects notes and add icon for unit menu item

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -18,29 +18,8 @@ const Home = () => {
           <TouchableOpacity
             activeOpacity={0.7}
             disabled={
-              item.title !== 'Zamanlar' &&
-              item.title !== 'Sorular' &&
-              item.title !== 'There is / There are' &&
-              item.title !== 'RC - NC' &&
-              item.title !== 'Passive' &&
-              item.title !== 'Bağlaçlar-1G' &&
-              item.title !== 'Bağlaçlar-2G' &&
-              item.title !== 'Bağlaçlar-3G' &&
-              item.title !== 'Comparative & Superlatives' &&
-              item.title !== 'Çiçek "To"' &&
-              item.title !== 'Şahsiyetsiz "It"' &&
-              item.title !== 'If Clauses' &&
-              item.title !== 'Prepositions' &&
-              item.title !== 'Modals' &&
-              item.title !== 'Zamirler' &&
-              item.title !== 'Belgisiz Zamirler' &&
-              item.title !== 'A, An, The' &&
-              item.title !== "'s vs of" &&
-              item.title !== 'Have / Has' &&
-              item.title !== 'Exciting / Excited' &&
-              item.title !== 'Emirler' &&
-              item.title !== 'Lets / Shall' &&
-              item.title !== 'Many, Much, Some, Any, A lot of'
+              item.title === 'Sıfatlar ve Zarflar' ||
+              item.title === 'Gerund & Infinitive'
             }
             onPress={() =>
               navigation.navigate('SubCategory', {
@@ -52,31 +31,10 @@ const Home = () => {
               styles.categoriesButton,
               {
                 backgroundColor:
-                  item.title === 'Zamanlar' ||
-                  item.title === 'Sorular' ||
-                  item.title === 'There is / There are' ||
-                  item.title === 'RC - NC' ||
-                  item.title === 'Passive' ||
-                  item.title === 'Bağlaçlar-1G' ||
-                  item.title === 'Bağlaçlar-2G' ||
-                  item.title === 'Bağlaçlar-3G' ||
-                  item.title === 'Comparative & Superlatives' ||
-                  item.title === 'Çiçek "To"' ||
-                  item.title === 'Şahsiyetsiz "It"' ||
-                  item.title === 'If Clauses' ||
-                  item.title === 'Prepositions' ||
-                  item.title === 'Modals' ||
-                  item.title === 'Zamirler' ||
-                  item.title === 'Belgisiz Zamirler' ||
-                  item.title === 'A, An, The' ||
-                  item.title === "'s vs of" ||
-                  item.title === 'Have / Has' ||
-                  item.title === 'Exciting / Excited' ||
-                  item.title === 'Emirler' ||
-                  item.title === 'Lets / Shall' ||
-                  item.title === 'Many, Much, Some, Any, A lot of'
-                    ? 'white'
-                    : '#f5f5f5',
+                  item.title === 'Sıfatlar ve Zarflar' ||
+                  item.title === 'Gerund & Infinitive'
+                    ? '#f5f5f5'
+                    : 'white',
               },
             ]}>
             <Image source={item.image} style={{width: 60, height: 60}} />
@@ -86,31 +44,10 @@ const Home = () => {
                   styles.categoriesTitle,
                   {
                     color:
-                      item.title === 'Zamanlar' ||
-                      item.title === 'Sorular' ||
-                      item.title === 'There is / There are' ||
-                      item.title === 'RC - NC' ||
-                      item.title === 'Passive' ||
-                      item.title === 'Bağlaçlar-1G' ||
-                      item.title === 'Bağlaçlar-2G' ||
-                      item.title === 'Bağlaçlar-3G' ||
-                      item.title === 'Comparative & Superlatives' ||
-                      item.title === 'Çiçek "To"' ||
-                      item.title === 'Şahsiyetsiz "It"' ||
-                      item.title === 'If Clauses' ||
-                      item.title === 'Prepositions' ||
-                      item.title === 'Zamirler' ||
-                      item.title === 'Modals' ||
-                      item.title === 'Belgisiz Zamirler' ||
-                      item.title === 'A, An, The' ||
-                      item.title === "'s vs of" ||
-                      item.title === 'Have / Has' ||
-                      item.title === 'Exciting / Excited' ||
-                      item.title === 'Emirler' ||
-                      item.title === 'Lets / Shall' ||
-                      item.title === 'Many, Much, Some, Any, A lot of'
-                        ? 'black'
-                        : 'darkgray',
+                      item.title === 'Sıfatlar ve Zarflar' ||
+                      item.title === 'Gerund & Infinitive'
+                        ? 'darkgray'
+                        : 'black',
                   },
                 ]}>
                 {item.title}
